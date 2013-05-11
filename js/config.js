8ff0ac20d90248db3d26421caa4b3d8d630123ee
Brent Jackson <jxnblk@gmail.com>
2013-05-11 15:24:11 -0700

    changed base tag to soundrad

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // SoundRad config
 
-var clientId = '66828e9e2042e682190d1fde4b02e265';
-var callbackUrl = 'http://soundrad.com/callback.html';
+var clientId = '683f27c0c6dace16e7498ebffcbef8be';
+var callbackUrl = 'http://soundrad.com/callback';
