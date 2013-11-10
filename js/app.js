e7681b4313f73dee6f0ee81c0936466d30203d59
Brent Jackson <jxnblk@gmail.com>
2013-11-10 17:48:28 -0500

    More routing

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ var soundrad = angular.module('soundrad', ['soundrad.services', 'soundrad.contro
     $stateProvider.state('playing', { url: "/playing", templateUrl: "/partials/playing.html" });
     
     $stateProvider.state('user', { url: "/:user", templateUrl: "/partials/user.html", controller: 'UserCtrl' });
-    $stateProvider.state('user.likes', { url: "/likes", templateUrl: "/partials/user.likes.html", controller: 'LikesCtrl' });
-    $stateProvider.state('user.sets', { url: "/sets", templateUrl: "/partials/user.sets.html", controller: 'SetsCtrl' });
-    $stateProvider.state('user.info', { url: "/info", templateUrl: "/partials/user.info.html", controller: 'InfoCtrl' });
-    $stateProvider.state('user.following', { url: "/following", templateUrl: "/partials/user.following.html", controller: 'FollowingCtrl' });
-    $stateProvider.state('user.followers', { url: "/followers", templateUrl: "/partials/user.followers.html", controller: 'FollowersCtrl' });
+        $stateProvider.state('user.likes', { url: "/likes", templateUrl: "/partials/user.likes.html", controller: 'LikesCtrl' });
+        $stateProvider.state('user.sets', { url: "/sets", templateUrl: "/partials/user.sets.html", controller: 'SetsCtrl' });
+        // $stateProvider.state('user.info', { url: "/info", templateUrl: "/partials/user.info.html", controller: 'InfoCtrl' });
+        // $stateProvider.state('user.following', { url: "/following", templateUrl: "/partials/user.following.html", controller: 'FollowingCtrl' });
+        // $stateProvider.state('user.followers', { url: "/followers", templateUrl: "/partials/user.followers.html", controller: 'FollowersCtrl' });
     $stateProvider.state('user.track', { url: "/:track", templateUrl: "/partials/user.track.html", controller: 'TrackDetailCtrl' });
     $stateProvider.state('user.set', { url: "/sets/:set", templateUrl: "/partials/user.sets.set.html", controller: 'SetDetailCtrl' });
 
