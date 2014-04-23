9ccf5ba536bec5a5e98298c70677aed9f47c8e38
jxnblk <jackson@kickstarter.com>
2014-04-23 14:06:55 -0400

    Adjust script includes

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 // SoundRad config
 
 // Beta config
-var clientID = '66828e9e2042e682190d1fde4b02e265';
-var callbackUrl = 'http://beta.soundrad.com/callback';
+//var clientID = '66828e9e2042e682190d1fde4b02e265';
+//var callbackUrl = 'http://beta.soundrad.com/callback';
 
 // Official config
-// var clientID = '683f27c0c6dace16e7498ebffcbef8be';
-// var callbackUrl = 'http://soundrad.com/callback';
+var clientID = '683f27c0c6dace16e7498ebffcbef8be';
+var callbackUrl = 'http://soundrad.com/callback';
