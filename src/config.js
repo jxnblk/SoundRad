6277c6dcf0dda2c3c8cea258b4d8299175f7561b
Brent Jackson <jacksonblack@gmail.com>
2014-10-19 16:08:16 -0400

    Fix sticky player

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 'use strict';
 
 // Beta config
-var clientID = '66828e9e2042e682190d1fde4b02e265';
-var callbackUrl = 'http://beta.soundrad.com';
+//var clientID = '66828e9e2042e682190d1fde4b02e265';
+//var callbackUrl = 'http://beta.soundrad.com';
 
 // Official config
-// var clientID = '683f27c0c6dace16e7498ebffcbef8be';
-// var callbackUrl = 'http://soundrad.com';
+var clientID = '683f27c0c6dace16e7498ebffcbef8be';
+var callbackUrl = 'http://soundrad.com';
